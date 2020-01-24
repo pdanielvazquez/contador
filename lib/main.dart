cdf7f22b519a4747a52e6ffd46a65e392619cabc
unknown <p.daniel.vazquez@gmail.com>
2020-01-24 15:44:25 -0600

    main.dart agregado

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:contador/src/app.dart';
+import 'package:flutter/material.dart';
+
+// Nuestras importaciones
+// import 'src/app.dart';
+
+void main(){
+
+  // ejecutar la App
+  runApp(new MyApp());
+
+}
+
